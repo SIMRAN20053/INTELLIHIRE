c8a7ae93af97c82af447fb4b9d1a8c8d07bfd5d6
Simran Lakhotia <simranlakhotia55@gmail.com>
2026-02-14 12:14:19 +0530

    Updating DASHBOARD UI

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import './dashboard.css';
 function Dashboard(){
     return(
         <>
-        <h1>Dashboard page</h1>
+      <div className="dashboard-page">
+        <div className="sidebar">
+            <h2 className="logo">IntelliHire</h2>
+<div className="menu">
+    <div>🏠 Home</div>
+    <div>➕ Post Job</div>
+    <div>📄 Applications</div>
+    <div>🧠 AI Insights</div>
+    <div>⚙️ Settings</div>
+    <div>🚪 Logout </div>
+</div>
+        </div>
+        <div className="main">
+        <div className="navbar">
+            <div className="nav-left">
+<h3 className="nav-logo">Dashboard</h3>
+            </div>
+<div className="nav-right">
+    <div className="bell">🔔</div>
+    <div className="avatar">JD</div>
+    <div className="username">John Doe</div>
+</div>
+</div>
+</div>
+      </div>
         </>
     )
 }
