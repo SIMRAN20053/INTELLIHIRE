8702b5f76c7bface298faffe34aa2b4e9e09292b
Simran Lakhotia <simranlakhotia55@gmail.com>
2026-06-17 19:28:08 +0530

    compelted the PyPDF2 module implementation

---
+++ backend/services/parser_resume.py
@@ -0,0 +1,9 @@
+from PyPDF2 import PdfReader
+def extract_text_from(file_path):
+    reader=PdfReader(file_path)
+    text=""
+    for page in reader.pages:
+        page_text=page.extract_text()
+        if page_text:
+            text+=page_text
+            return text
